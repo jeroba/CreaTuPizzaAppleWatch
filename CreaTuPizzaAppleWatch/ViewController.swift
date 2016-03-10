888acb1a21540f55014b448a5817ebfd36cf612f
Jesus Rodriguez Barrera <jesus@macbook-pro-de-jesus.local>
2016-03-10 00:34:38 -0600

    Initial Commit

---
+++ CreaTuPizzaAppleWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreaTuPizzaAppleWatch
+//
+//  Created by Jesus Rodriguez Barrera on 10/03/16.
+//  Copyright © 2016 Aplicapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
